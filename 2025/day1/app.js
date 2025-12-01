1ae9f39ebc7b8aa765a7bdee97abb9722a3ded81
David Genelid <david.genelid@valtech.com>
2025-12-01 15:10:26 +0100

    Add first day, first stupid version

---
+++ 2025/day1/app.js
@@ -0,0 +1,18 @@
+// First stupid version
+const fs = require('fs')
+
+let numZero = 0
+let current = 50
+
+const input = fs.readFileSync('input', 'utf-8')
+input.split(/\n/).forEach(line =>  {
+  if (line) {
+    const num = parseInt(line.substr(1), 10)
+    const s = line.charAt(0) === 'L' ? -num : +num
+    current = (current + s) % 100
+    if (current < 0) current += 100
+    if (current === 0) numZero++
+  }
+})
+
+console.log('Answer:', numZero)
