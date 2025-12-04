45bd41757272bab82fad48bc83a2e0f8bc09335a
David Genelid <david.genelid@valtech.com>
2025-12-04 14:21:14 +0100

    Add day 4 part 1 solution

---
+++ 2025/day4/app.js
@@ -0,0 +1,28 @@
+// Simple version
+console.time()
+const fs = require('fs')
+
+const input = fs.readFileSync('input', 'utf-8')
+let answer = 0
+
+const grid = input.trim().split(/\n/).map(line => line.split(''))
+for (let y = 0; y < grid.length; y++) {
+  for (let x = 0; x < grid[y].length; x++) {
+    if (grid[y][x] === '@') {
+      let count = 0
+      for (let cy = y - 1; cy <= y + 1; cy++) {
+        for (let cx = x - 1; cx <= x + 1; cx++) {
+          if (cy === y && cx === x) continue
+          if (grid?.[cy]?.[cx] === '@') {
+            count++
+          }
+        }
+        if (count >= 4) break
+      }
+      if (count < 4) answer++
+    }
+  }
+}
+
+console.log('Answer:', answer)
+console.timeEnd()
