0128fafdbc8370c3913f058ccb26628e93257e1c
David Genelid <david.genelid@valtech.com>
2025-12-09 16:29:05 +0100

    Add day 6 part 2 solution, remove test files

--- 2025/day6/app2.js
+++ 2025/day6/app2.js
@@ -2,29 +2,32 @@
 console.time()
 const fs = require('fs')
 
-const input = fs.readFileSync('input-test', 'utf-8')
+const input = fs.readFileSync('input', 'utf-8')
 let answer = 0
 
-const lines = input.trim().split(/\n/).map((line, index, a) => {
-  const cols = line.trim().split(/\s+/)
-  return index === a.length - 1 ? cols : cols.map(v => parseInt(v, 10))
+const lines = input.split(/\n/)
+const symbols = input
+  .match(/([\*\+]\s+)/g)
+
+let x = 0
+symbols.forEach((s, i) => {
+  const l = s.length - 1
+  const c = new Array(l).fill('')
+  for (let j = 0; j < l; j++) {
+    for (let k = 0; k < lines.length - 2; k++) {
+      const v = lines[k].substring(x + j, x + j + 1).trim()
+      c[j] += v 
+    }
+  }
+
+  const multi = s.includes('*')
+  const total = c.map(v => parseInt(v, 10)).reduce((a, cv) => {
+    return multi ? a * cv : a + cv
+  }, multi ? 1 : 0) 
+
+  answer += total
+  x += s.length
 })
-
-console.log(lines)
-
-// const symbols = lines[lines.length - 1]
-// for (let col = 0; col < lines[0].length; col++) {
-//   let a = lines[0][col]
-//   let str = ''
-//   for (let row = 1; row < lines.length - 1; row++) {
-//     str += lines[row][col]
-//     str += symbols[col]
-//     if (symbols[col] === '*') a *= lines[row][col]
-//     else a += lines[row][col]
-//   }
-//   console.log(str, '=', a)
-//   answer += a
-// }
 
 console.log('Answer:', answer)
 console.timeEnd()
