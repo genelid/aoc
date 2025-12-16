cf26f0e65bd4f29dff17c80781c21e11cb43cc32
David Genelid <david@weareyours.com>
2025-12-17 00:39:07 +0100

    Update day 9 part 2 WIP

--- 2025/day09/app2.js
+++ 2025/day09/app2.js
@@ -13,21 +13,30 @@ const tiles = input.trim().split(/\n/)
 
 console.log(tiles)
 
+const map = new Map()
+tiles.forEach(t => {
+  map.set(t.x + '.' + t.y, true)
+})
+
 let answer = 0
 
 tiles.forEach((a, i) => {
-  // console.log(i)
+  console.log(i)
   tiles.forEach((b, j) => {
     if (j > i) {
       const w = Math.abs(b.x - (a.x - 1))
       const h = Math.abs(b.y - (a.y - 1))
       const area = w * h
+      const aa = a.x < b.x ? a.x + w : a.x - w
+      const bb = a.y < b.y ? a.y + h : a.y - h
+      // TODO: Check if aa and bb is inside
+      console.log('j:', j, 'w:', w, 'h:', h, 'area:', area)
       if (area > answer) answer = area
-      // console.log('j:', j, 'w:', w, 'h:', h, 'area:', area)
     }
   })
 })
 
 console.log('Answer:', answer)
 console.timeEnd()
+
 
